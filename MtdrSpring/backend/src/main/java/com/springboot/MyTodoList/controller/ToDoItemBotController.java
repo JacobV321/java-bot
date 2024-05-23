5763f0ae9ca2af4ad81ae2e7e5e2a2ee94db3f48
Carlos Rojas <carlosluzumbero@gmail.com>
2024-05-22 18:18:04 -0700

    test1
    test1

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/ToDoItemBotController.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/ToDoItemBotController.java
@@ -1,4 +1,5 @@
 package com.springboot.MyTodoList.controller;
+
 import com.springboot.MyTodoList.controller.UserAuthentication;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
@@ -287,93 +288,341 @@ import com.springboot.MyTodoList.util.BotMessages;
 		
 	} */
 
-	public class ToDoItemBotController extends TelegramLongPollingBot {
+public class ToDoItemBotController extends TelegramLongPollingBot {
 
-		private static final Logger logger = LoggerFactory.getLogger(ToDoItemBotController.class);
-		private ToDoItemService toDoItemService;
-		private String botName;
-		private UserAuthentication userAuthentication;
-	
-		public ToDoItemBotController(String botToken, String botName, ToDoItemService toDoItemService, UserAuthentication userAuthentication) {
-			super(botToken);
-			logger.info("Bot Token: " + botToken);
-			logger.info("Bot name: " + botName);
-			this.toDoItemService = toDoItemService;
-			this.botName = botName;
-			this.userAuthentication = userAuthentication;
-		}
-	
-		@Override
-		public void onUpdateReceived(Update update) {
-			if (update.hasMessage() && update.getMessage().hasText()) {
-				String messageTextFromTelegram = update.getMessage().getText();
-				long chatId = update.getMessage().getChatId();
-	
-				if (messageTextFromTelegram.equals(BotCommands.START_COMMAND.getCommand())) {
-					// Solo permitir el comando /login al iniciar el bot
-					SendMessage messageToTelegram = new SendMessage();
-					messageToTelegram.setChatId(chatId);
-					messageToTelegram.setText("Por favor, introduce /login para autenticarte.");
-					try {
-						execute(messageToTelegram);
-					} catch (TelegramApiException e) {
-						logger.error(e.getLocalizedMessage(), e);
-					}
-				} else if (messageTextFromTelegram.startsWith(BotCommands.LOG_IN.getCommand())) {
-					// Lógica de autenticación
-					String[] parts = messageTextFromTelegram.split("\\s+", 3); // Divide en al menos 3 partes, ignorando los espacios extras
-					if (parts.length != 3) {
-						sendErrorMessage(chatId, "Por favor, introduce tu nombre de usuario y contraseña en el siguiente formato: /login usuario contraseña");
-						return;
-					}
-					String username = parts[1];
-					String password = parts[2];
-					String[] authenticationResult = userAuthentication.isAuthenticated(username, password);
-					if (authenticationResult[0].equals("true")) {
-						String name = authenticationResult[1];
-						String role = authenticationResult[2];
-						sendSuccessMessage(chatId, "¡Hola " + name + "! Eres un " + role);
-					} else {
-						sendErrorMessage(chatId, authenticationResult[1]);
-					}
-				}
-				 else {
-					// Usuario no ha iniciado sesión
-					sendErrorMessage(chatId, "Por favor, inicia sesión primero con /login");
-				}
-				
-			}
-		}
-	
-		private void sendSuccessMessage(Long chatId, String text) {
-			SendMessage message = new SendMessage();
-			message.setChatId(chatId);
-			message.setText(text);
-			try {
-				execute(message);
-			} catch (TelegramApiException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		private void sendErrorMessage(Long chatId, String text) {
-			SendMessage message = new SendMessage();
-			message.setChatId(chatId);
-			message.setText(text);
-			try {
-				execute(message);
-			} catch (TelegramApiException e) {
-				e.printStackTrace();
-			}
-		}
-	
+	private static final Logger logger = LoggerFactory.getLogger(ToDoItemBotController.class);
+	private ToDoItemService toDoItemService;
+	private String botName;
+	private UserAuthentication userAuthentication;
+
+	public ToDoItemBotController(String botToken, String botName, ToDoItemService toDoItemService,
+			UserAuthentication userAuthentication) {
+		super(botToken);
+		logger.info("Bot Token: " + botToken);
+		logger.info("Bot name: " + botName);
+		this.toDoItemService = toDoItemService;
+		this.botName = botName;
+		this.userAuthentication = userAuthentication;
+	}
+
 	@Override
-	public String getBotUsername() {		
+	public void onUpdateReceived(Update update) {
+		if (update.hasMessage() && update.getMessage().hasText()) {
+			String messageTextFromTelegram = update.getMessage().getText();
+			long chatId = update.getMessage().getChatId();
+
+			if (messageTextFromTelegram.equals(BotCommands.START_COMMAND.getCommand())) {
+				// Solo permitir el comando /login al iniciar el bot
+				SendMessage messageToTelegram = new SendMessage();
+				messageToTelegram.setChatId(chatId);
+				messageToTelegram.setText("Por favor, introduce /login para autenticarte.");
+				try {
+					execute(messageToTelegram);
+				} catch (TelegramApiException e) {
+					logger.error(e.getLocalizedMessage(), e);
+				}
+			} else if (messageTextFromTelegram.startsWith(BotCommands.LOG_IN.getCommand())) {
+				// Lógica de autenticación
+				String[] parts = messageTextFromTelegram.split("\\s+", 3); // Divide en al menos 3 partes, ignorando los
+																			// espacios extras
+				if (parts.length != 3) {
+					sendErrorMessage(chatId,
+							"Por favor, introduce tu nombre de usuario y contraseña en el siguiente formato: /login usuario contraseña");
+					return;
+				}
+				String username = parts[1];
+				String password = parts[2];
+				String[] authenticationResult = userAuthentication.isAuthenticated(username, password);
+				if (authenticationResult[0].equals("true")) {
+					String name = authenticationResult[1];
+					String role = authenticationResult[2];
+					sendSuccessMessage(chatId, "¡Hola " + name + "! Eres un " + role);
+					handleUserCommands(chatId, messageTextFromTelegram, role);
+				} else {
+					sendErrorMessage(chatId, authenticationResult[1]);
+				}
+			} else {
+				// Usuario no ha iniciado sesión
+				sendErrorMessage(chatId, "Por favor, inicia sesión primero con /login");
+			}
+
+		}
+	}
+
+	private void handleUserCommands(long chatId, String messageTextFromTelegram, String role) {
+		if (role.equals("dev")) {
+			if (messageTextFromTelegram.equals(BotCommands.START_COMMAND.getCommand())) {
+				handleStartCommand(chatId);
+			} else if (messageTextFromTelegram.contains(BotLabels.DONE.getLabel())) {
+				handleDoneCommand(chatId, messageTextFromTelegram);
+			} else if (messageTextFromTelegram.contains(BotLabels.UNDO.getLabel())) {
+				handleUndoCommand(chatId, messageTextFromTelegram);
+			} else if (messageTextFromTelegram.contains(BotLabels.DELETE.getLabel())) {
+				handleDeleteCommand(chatId, messageTextFromTelegram);
+			} else if (messageTextFromTelegram.equals(BotCommands.HIDE_COMMAND.getCommand())
+					|| messageTextFromTelegram.equals(BotLabels.HIDE_MAIN_SCREEN.getLabel())) {
+				handleHideCommand(chatId);
+			} else if (messageTextFromTelegram.equals(BotCommands.TODO_LIST.getCommand())
+					|| messageTextFromTelegram.equals(BotLabels.LIST_ALL_ITEMS.getLabel())
+					|| messageTextFromTelegram.equals(BotLabels.MY_TODO_LIST.getLabel())) {
+				handleToDoListCommand(chatId);
+			} else if (messageTextFromTelegram.equals(BotCommands.ADD_ITEM.getCommand())
+					|| messageTextFromTelegram.equals(BotLabels.ADD_NEW_ITEM.getLabel())) {
+				handleAddItemCommand(chatId);
+			} else {
+				handleNewItem(chatId, messageTextFromTelegram);
+			}
+		} else if (role.equals("admin")) {
+			if (messageTextFromTelegram.equals(BotLabels.TEAM_LIST.getLabel())) {
+				handleTeamListCommand(chatId);
+			} else {
+				sendErrorMessage(chatId, "Comando no reconocido para el rol admin.");
+			}
+		} else {
+			sendErrorMessage(chatId, "Rol no reconocido.");
+		}
+	}
+
+	public void handleDevCommand(long chatId) {
+		SendMessage messageToTelegram = new SendMessage();
+		messageToTelegram.setChatId(chatId);
+		messageToTelegram.setText("Dev Options:");
+
+		ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+		List<KeyboardRow> keyboard = new ArrayList<>();
+
+		KeyboardRow row1 = new KeyboardRow();
+		row1.add(BotLabels.LIST_ALL_ITEMS.getLabel());
+		keyboard.add(row1);
+
+		KeyboardRow row2 = new KeyboardRow();
+		row2.add(BotLabels.SHOW_MAIN_SCREEN.getLabel());
+		row2.add(BotLabels.HIDE_MAIN_SCREEN.getLabel());
+		keyboard.add(row2);
+
+		keyboardMarkup.setKeyboard(keyboard);
+		messageToTelegram.setReplyMarkup(keyboardMarkup);
+
+		try {
+			execute(messageToTelegram);
+		} catch (TelegramApiException e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+
+	public void handleManagerCommand(long chatId) {
+		SendMessage messageToTelegram = new SendMessage();
+		messageToTelegram.setChatId(chatId);
+		messageToTelegram.setText("Opciones de Manager:");
+
+		ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+		List<KeyboardRow> keyboard = new ArrayList<>();
+
+		// Añadir el botón para TEAM_LIST
+		KeyboardRow row1 = new KeyboardRow();
+		row1.add(BotLabels.TEAM_LIST.getLabel());
+		keyboard.add(row1);
+
+		keyboardMarkup.setKeyboard(keyboard);
+		messageToTelegram.setReplyMarkup(keyboardMarkup);
+
+		try {
+			execute(messageToTelegram);
+		} catch (TelegramApiException e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+
+	public void handleTeamListCommand(long chatId) {
+		SendMessage messageToTelegram = new SendMessage();
+		messageToTelegram.setChatId(chatId);
+		messageToTelegram.setText("Lista de equipo");
+
+		try {
+			execute(messageToTelegram);
+		} catch (TelegramApiException e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+
+	public void handleStartCommand(long chatId) {
+		SendMessage messageToTelegram = new SendMessage();
+		messageToTelegram.setChatId(chatId);
+		messageToTelegram.setText(BotMessages.HELLO_MYTODO_BOT.getMessage());
+
+		ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+		List<KeyboardRow> keyboard = new ArrayList<>();
+
+		keyboardMarkup.setKeyboard(keyboard);
+		messageToTelegram.setReplyMarkup(keyboardMarkup);
+
+		try {
+			execute(messageToTelegram);
+		} catch (TelegramApiException e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+
+	public void handleDoneCommand(long chatId, String messageTextFromTelegram) {
+		String done = messageTextFromTelegram.substring(0, messageTextFromTelegram.indexOf(BotLabels.DASH.getLabel()));
+		Integer id = Integer.valueOf(done);
+
+		try {
+			ToDoItem item = getToDoItemById(id).getBody();
+			item.setDone(true);
+			updateToDoItem(item, id);
+			BotHelper.sendMessageToTelegram(chatId, BotMessages.ITEM_DONE.getMessage(), this);
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+
+	public void handleUndoCommand(long chatId, String messageTextFromTelegram) {
+		String undo = messageTextFromTelegram.substring(0, messageTextFromTelegram.indexOf(BotLabels.DASH.getLabel()));
+		Integer id = Integer.valueOf(undo);
+
+		try {
+			ToDoItem item = getToDoItemById(id).getBody();
+			item.setDone(false);
+			updateToDoItem(item, id);
+			BotHelper.sendMessageToTelegram(chatId, BotMessages.ITEM_UNDONE.getMessage(), this);
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+
+	public void handleDeleteCommand(long chatId, String messageTextFromTelegram) {
+		String delete = messageTextFromTelegram.substring(0,
+				messageTextFromTelegram.indexOf(BotLabels.DASH.getLabel()));
+		Integer id = Integer.valueOf(delete);
+
+		try {
+			deleteToDoItem(id).getBody();
+			BotHelper.sendMessageToTelegram(chatId, BotMessages.ITEM_DELETED.getMessage(), this);
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+
+	public void handleHideCommand(long chatId) {
+		BotHelper.sendMessageToTelegram(chatId, BotMessages.BYE.getMessage(), this);
+	}
+
+	public void handleToDoListCommand(long chatId) {
+		List<ToDoItem> allItems = getAllToDoItems();
+		ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+		List<KeyboardRow> keyboard = new ArrayList<>();
+
+		KeyboardRow mainScreenRowTop = new KeyboardRow();
+		mainScreenRowTop.add(BotLabels.SHOW_MAIN_SCREEN.getLabel());
+		keyboard.add(mainScreenRowTop);
+
+		KeyboardRow firstRow = new KeyboardRow();
+		firstRow.add(BotLabels.ADD_NEW_ITEM.getLabel());
+		keyboard.add(firstRow);
+
+		KeyboardRow myTodoListTitleRow = new KeyboardRow();
+		myTodoListTitleRow.add(BotLabels.MY_TODO_LIST.getLabel());
+		keyboard.add(myTodoListTitleRow);
+
+		List<ToDoItem> activeItems = allItems.stream().filter(item -> !item.isDone()).collect(Collectors.toList());
+
+		for (ToDoItem item : activeItems) {
+			KeyboardRow currentRow = new KeyboardRow();
+			currentRow.add(item.getDescription());
+			currentRow.add(item.getID() + BotLabels.DASH.getLabel() + BotLabels.DONE.getLabel());
+			keyboard.add(currentRow);
+		}
+
+		List<ToDoItem> doneItems = allItems.stream().filter(ToDoItem::isDone).collect(Collectors.toList());
+
+		for (ToDoItem item : doneItems) {
+			KeyboardRow currentRow = new KeyboardRow();
+			currentRow.add(item.getDescription());
+			currentRow.add(item.getID() + BotLabels.DASH.getLabel() + BotLabels.UNDO.getLabel());
+			currentRow.add(item.getID() + BotLabels.DASH.getLabel() + BotLabels.DELETE.getLabel());
+			keyboard.add(currentRow);
+		}
+
+		KeyboardRow mainScreenRowBottom = new KeyboardRow();
+		mainScreenRowBottom.add(BotLabels.SHOW_MAIN_SCREEN.getLabel());
+		keyboard.add(mainScreenRowBottom);
+
+		keyboardMarkup.setKeyboard(keyboard);
+
+		SendMessage messageToTelegram = new SendMessage();
+		messageToTelegram.setChatId(chatId);
+		messageToTelegram.setText(BotLabels.MY_TODO_LIST.getLabel());
+		messageToTelegram.setReplyMarkup(keyboardMarkup);
+
+		try {
+			execute(messageToTelegram);
+		} catch (TelegramApiException e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+
+	public void handleAddItemCommand(long chatId) {
+		try {
+			SendMessage messageToTelegram = new SendMessage();
+			messageToTelegram.setChatId(chatId);
+			messageToTelegram.setText(BotMessages.TYPE_NEW_TODO_ITEM.getMessage());
+			ReplyKeyboardRemove keyboardMarkup = new ReplyKeyboardRemove(true);
+			messageToTelegram.setReplyMarkup(keyboardMarkup);
+
+			execute(messageToTelegram);
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+
+	public void handleNewItem(long chatId, String messageTextFromTelegram) {
+		try {
+			ToDoItem newItem = new ToDoItem();
+			newItem.setDescription(messageTextFromTelegram);
+			newItem.setCreation_ts(OffsetDateTime.now());
+			newItem.setDone(false);
+			addToDoItem(newItem);
+
+			SendMessage messageToTelegram = new SendMessage();
+			messageToTelegram.setChatId(chatId);
+			messageToTelegram.setText(BotMessages.NEW_ITEM_ADDED.getMessage());
+
+			execute(messageToTelegram);
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+
+	private void sendSuccessMessage(Long chatId, String text) {
+		SendMessage message = new SendMessage();
+		message.setChatId(chatId);
+		message.setText(text);
+		try {
+			execute(message);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void sendErrorMessage(Long chatId, String text) {
+		SendMessage message = new SendMessage();
+		message.setChatId(chatId);
+		message.setText(text);
+		try {
+			execute(message);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public String getBotUsername() {
 		return botName;
 	}
 
 	// GET /todolist
-	public List<ToDoItem> getAllToDoItems() { 
+	public List<ToDoItem> getAllToDoItems() {
 		return toDoItemService.findAll();
 	}
 
--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotLabels.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotLabels.java
@@ -1,18 +1,17 @@
 package com.springboot.MyTodoList.util;
 
 public enum BotLabels {
-	
-	SHOW_MAIN_SCREEN("Show Main Screen"), 
+
+	SHOW_MAIN_SCREEN("Show Main Screen"),
 	HIDE_MAIN_SCREEN("Hide Main Screen"),
-	LIST_ALL_ITEMS("List All Items"), 
+	LIST_ALL_ITEMS("List All Items"),
 	ADD_NEW_ITEM("Add New Item"),
 	DONE("DONE"),
 	UNDO("UNDO"),
 	DELETE("DELETE"),
 	MY_TODO_LIST("MY TODO LIST"),
 	DASH("-"),
-	MANAGER("MANAGER"),
-	DEV("DEV");
+	TEAM_LIST("TEAM LIST");
 
 	private String label;
 
